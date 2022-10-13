dd3123a83847656b57a52a11169533791a75d49c
Yong Gyun Kim <edward@edykim.com>
2022-10-13 16:37:27 -0700

    Add test for array reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,56 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceWithArray() {
+    int[] arrayOfNumbers = { 1, 2, 3, 4, 5 };
+    int[] expected = { 5, 4, 3, 2, 1 };
+    ArrayExamples.reverseInPlace(arrayOfNumbers);
+
+    assertArrayEquals(expected, arrayOfNumbers);
+  }
+
+  @Test
+  public void testReversedWithArray() {
+    int[] arrayOfNumbers = { 1, 2, 3, 4, 5 };
+    int[] expected = { 5, 4, 3, 2, 1 };
+    int[] actual = ArrayExamples.reversed(arrayOfNumbers);
+
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] numbers = {-10, 1, 2, 3, 4, 5};
+    double expected = 3;
+    double actual = ArrayExamples.averageWithoutLowest(numbers);
+
+    assertEquals(expected, actual, 0);
+  }
+
+  @Test
+  public void testAverageWithoutLowestWithEmpty() {
+    double[] numbers = {};
+    double expected = 0;
+    double actual = ArrayExamples.averageWithoutLowest(numbers);
+
+    assertEquals(expected, actual, 0);
+  }
+  @Test
+  public void testAverageWithoutLowestWithOne() {
+    double[] numbers = {10};
+    double expected = 0;
+    double actual = ArrayExamples.averageWithoutLowest(numbers);
+
+    assertEquals(expected, actual, 0);
+  }
+  @Test
+  public void testAverageWithoutLowestWithDuplication() {
+    double[] numbers = {10, 10, 10, 10, 12};
+    double expected = 12;
+    double actual = ArrayExamples.averageWithoutLowest(numbers);
+
+    assertEquals(expected, actual, 0);
+  }
 }
